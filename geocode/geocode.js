f2413a4d69eaa3c6d093af0261e4a499d4dfb909
Zachary <zak.d.grant@gmail.com>
2018-08-23 09:49:28 -0400

    changed location api from google to mapquest

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,24 +1,27 @@
-const request = require('request');
+const request = require("request");
 
 var geocodeAddress = (address, callback) => {
   var encodedAddress = encodeURIComponent(address);
 
-  request({
-    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-  }, (error, response, body) => {
-    if (error) {
-      callback('Unable to connect to Google servers');
-    } else if (body.status === 'ZERO_RESULTS') {
-      callback('Unable to find that address');
-    } else if (body.status === 'OK') {
-      callback(undefined, {
-        address: body.results[0].formatted_address,
-        latitude: body.results[0].geometry.location.lat,
-        longitude: body.results[0].geometry.location.lng
-      });
+  request(
+    {
+      url: `http://www.mapquestapi.com/geocoding/v1/address?key=O6zILA52TDGWHgdrKSXJSHrnQgZkeiL7&location=${encodedAddress}`,
+      json: true
+    },
+    (error, response, body) => {
+      if (error) {
+        callback("Unable to connect to Google servers");
+      } else if (body.status === "ZERO_RESULTS") {
+        callback("Unable to find that address");
+      } else if (body.status === "OK") {
+        callback(undefined, {
+          address: body.results[0].formatted_address,
+          latitude: body.results[0].locations[0].latLng.lat,
+          longitude: body.results[0].locations[0].latLng.lng
+        });
+      }
     }
-  });
+  );
 };
 
 module.exports.geocodeAddress = geocodeAddress;
